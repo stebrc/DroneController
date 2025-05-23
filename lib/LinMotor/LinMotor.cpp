d79de52554ecfca801ff8507a669f305fd91d540
stebrc <stefanuzzo22@gmail.com>
2025-05-24 00:46:48 +0200

    Refactor: streamline motor control logic using applyControl function for improved readability and maintainability

--- lib/LinMotor/LinMotor.cpp
+++ lib/LinMotor/LinMotor.cpp
@@ -6,53 +6,27 @@ void setMotor(int extendPin, int retractPin, int extendState, int retractState) 
 }
 
 void controlMotors(float rollPID, float pitchPID) {
-    // --- Gamba A ---
-    if (!digitalRead(SWITCH_A)) {
-        setMotor(MOTOR_A_EXTEND, MOTOR_A_RETRACT, 1, 0); // Forza estensione
-    } else {
-        if (rollPID > 0)
-            setMotor(MOTOR_A_EXTEND, MOTOR_A_RETRACT, 0, 1); // Estensione (inverso)
-        else if (rollPID < 0)
-            setMotor(MOTOR_A_EXTEND, MOTOR_A_RETRACT, 1, 0); // Ritrazione
-        else
-            setMotor(MOTOR_A_EXTEND, MOTOR_A_RETRACT, 0, 0); // Stop
-    }
+    auto applyControl = [](int sw, float pid, int ext, int ret, bool invert = false) {
+        int extend = 0, retract = 0;
+        if (!digitalRead(sw)) {
+            extend = 1;
+            retract = 0;
+        } else {
+            if (pid > 0) {
+                extend = invert ? 0 : 1;
+                retract = invert ? 1 : 0;
+            } else if (pid < 0) {
+                extend = invert ? 1 : 0;
+                retract = invert ? 0 : 1;
+            }
+        }
+        setMotor(ext, ret, extend, retract);
+    };
 
-    // --- Gamba B ---
-    if (!digitalRead(SWITCH_B)) {
-        setMotor(MOTOR_B_EXTEND, MOTOR_B_RETRACT, 1, 0);
-    } else {
-        if (pitchPID > 0)
-            setMotor(MOTOR_B_EXTEND, MOTOR_B_RETRACT, 1, 0);
-        else if (pitchPID < 0)
-            setMotor(MOTOR_B_EXTEND, MOTOR_B_RETRACT, 0, 1);
-        else
-            setMotor(MOTOR_B_EXTEND, MOTOR_B_RETRACT, 0, 0);
-    }
-
-    // --- Gamba C ---
-    if (!digitalRead(SWITCH_C)) {
-        setMotor(MOTOR_C_EXTEND, MOTOR_C_RETRACT, 1, 0);
-    } else {
-        if (rollPID > 0)
-            setMotor(MOTOR_C_EXTEND, MOTOR_C_RETRACT, 1, 0);
-        else if (rollPID < 0)
-            setMotor(MOTOR_C_EXTEND, MOTOR_C_RETRACT, 0, 1);
-        else
-            setMotor(MOTOR_C_EXTEND, MOTOR_C_RETRACT, 0, 0);
-    }
-
-    // --- Gamba D ---
-    if (!digitalRead(SWITCH_D)) {
-        setMotor(MOTOR_D_EXTEND, MOTOR_D_RETRACT, 1, 0);
-    } else {
-        if (pitchPID > 0)
-            setMotor(MOTOR_D_EXTEND, MOTOR_D_RETRACT, 0, 1); // Inverso
-        else if (pitchPID < 0)
-            setMotor(MOTOR_D_EXTEND, MOTOR_D_RETRACT, 1, 0);
-        else
-            setMotor(MOTOR_D_EXTEND, MOTOR_D_RETRACT, 0, 0);
-    }
+    applyControl(SWITCH_A, rollPID, MOTOR_A_EXTEND, MOTOR_A_RETRACT, true);
+    applyControl(SWITCH_B, pitchPID, MOTOR_B_EXTEND, MOTOR_B_RETRACT);
+    applyControl(SWITCH_C, rollPID, MOTOR_C_EXTEND, MOTOR_C_RETRACT);
+    applyControl(SWITCH_D, pitchPID, MOTOR_D_EXTEND, MOTOR_D_RETRACT, true);
 }
 
 void extendUntilContact(bool &vola, bool &atterra) {
